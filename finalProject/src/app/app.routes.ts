b54d85a4aded2276e7f4584a64b36785740bf208
Kriti Agrawal <kritiagrawal09@gmail.com>
2024-04-01 17:16:52 +0530

    Initial Commit

--- finalProject/src/app/app.routes.ts
+++ finalProject/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'register',
         component: RegisterComponent,
+    },
+    {
+        path: 'login/forgot',
+        component: ForgotPasswordComponent,
     }
 ];
